bb136fa4e93be46d11bdec79cc94c019659ee43d
Monil Ghadiya <monil.ghadiya@gmail.com>
2021-02-05 16:14:08 +0530

    Initial commit

---
+++ src/main/java/com/myExpense/myExpenseTracker/MyExpenseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.myExpense.myExpenseTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyExpenseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyExpenseTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myExpense/myExpenseTracker/MyExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myExpense.myExpenseTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
